4d3eb51cc57f7973bd6b2c683686917a2fef8f76
Suchitra Naik <susanaik@gmail.com>
2018-06-04 13:44:07 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -20,7 +20,10 @@ class InteractiveRecord
       column_names.compact
   end
 
-  def initialize
+  def initialize(params = {})
+    params.each do |property, value|
+      self.send("#{property}=", value)
+    end
   end
 
   def save
